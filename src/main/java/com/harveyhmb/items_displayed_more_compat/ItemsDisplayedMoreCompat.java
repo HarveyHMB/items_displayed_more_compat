9476b95ca86f7ce9e459951182d8ef274d8cd660
Mats Boekhorst <mats.boekhorst@gmail.com>
2026-07-01 20:25:49 +0200

    Initial commit

---
+++ src/main/java/com/harveyhmb/items_displayed_more_compat/ItemsDisplayedMoreCompat.java
@@ -0,0 +1,12 @@
+package com.harveyhmb.items_displayed_more_compat;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ItemsDisplayedMoreCompat implements ModInitializer {
+    public static final String MOD_ID = "items_displayed_more_compat";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/harveyhmb/items_displayed_more_compat/client/ItemsDisplayedMoreCompatClient.java
@@ -0,0 +1,10 @@
+package com.harveyhmb.items_displayed_more_compat.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ItemsDisplayedMoreCompatClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
